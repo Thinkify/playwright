55b7245ecbe567578bcdc32a01b95990dcd81965
pavantekal <pavantekal@thinkify.io>
2021-10-27 17:59:33 +0530

    first commit

---
+++ playwright.config.js
@@ -0,0 +1,55 @@
+// @ts-check
+
+const { devices } = require('@playwright/test');
+
+const config = {
+    use: {
+      browserName: 'chromium',
+      headless: false,
+      viewport: { width: 1280, height: 720 },
+      ignoreHTTPSErrors: true,
+      video: 'on-first-retry',
+      screenshot: 'on',
+      trace: 'on',
+      testDir: 'tests/scenarios',
+      timeout: 30000,
+
+      //baseURL: 'http://localhost:3000',
+    },
+    // projects: [
+    //     {
+    //       name: 'Chromium',
+    //       use: {
+    //         // Configure the browser to use.
+    //         browserName: 'chromium',
+    
+    //         // Any Chromium-specific options.
+    //         viewport: { width: 600, height: 800 },
+    //       },
+    //     },
+    
+    //     {
+    //       name: 'Firefox',
+    //       use: { browserName: 'firefox' },
+    //     },
+    
+    //     {
+    //       name: 'WebKit',
+    //       use: { browserName: 'webkit' },
+    //     },
+    //           // Test against mobile viewports.
+    //     {
+    //     name: 'Mobile Chrome',
+    //     use: devices['Pixel 5'],
+    //     },
+    //     {
+    //         name: 'Mobile Safari',
+    //         use: devices['iPhone 12'],
+    //       },
+      
+    //   ],
+    // "scripts": {
+    //     "test": "playwright test"
+    // }
+};
+  module.exports = config;
